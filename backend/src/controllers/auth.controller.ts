856f2dec1270caff179c09aa35fe03b5d4f55887
preethiviy <preethivirajyambem@gmail.com>
2024-09-08 01:20:05 +0530

    login, sign tokens code refactored

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,25 +1,13 @@
-import { z } from "zod";
 import catchErrors from "../utils/catchErrors";
-import { createAccount } from "../services/auth.service";
-import { CREATED } from "../constants/http";
+import { createAccount, loginUser } from "../services/auth.service";
+import { CREATED, OK } from "../constants/http";
 import { setAuthCookies } from "../utils/cookies";
-
-const resgisterSchema = z.object({
-    email: z.string().email().min(1).max(255),
-    password: z.string().min(6).max(255),
-    confirmPassword: z.string().min(6).max(255),
-    userAgent: z.string().optional()
-}).refine(
-    (data) => data.password === data.confirmPassword, {
-        message: "Passwords do not match",
-        path: ["confirmPassword"]
-    }
-)
+import { loginSchema, registerSchema } from "./auth.schemas";
 
 export const registerHandler = catchErrors(
     async (req, res) => {
         // validate request
-        const request = resgisterSchema.parse({
+        const request = registerSchema.parse({
             ...req.body,
             userAgent: req.headers["user-agent"]
         })
@@ -31,5 +19,23 @@ export const registerHandler = catchErrors(
         return setAuthCookies({res, accessToken, refreshToken})
         .status(CREATED)
         .json(user);
+    }
+)
+
+export const loginHandler = catchErrors(
+    async (req, res) => {
+        // validate request
+        const request = loginSchema.parse({
+            ...req.body,
+            userAgent: req.headers["user-agent"]
+        })
+
+        // call service
+        const {accessToken, refreshToken} = await loginUser(request);
+
+        // return response
+        return setAuthCookies({res, accessToken, refreshToken})
+        .status(OK)
+        .json({message: "Login Successful"});
     }
 )
--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { JWT_REFRESH_SECRET, JWT_SECRET } from "../constants/env";
-import { CONFLICT } from "../constants/http";
+import { CONFLICT, UNAUTHORIZED } from "../constants/http";
 import VerificationCodeType from "../constants/verificationCodeTypes";
 import SessionModel from "../models/session.model";
 import UserModel from "../models/user.model";
@@ -7,6 +7,7 @@ import VerificationCodeModel from "../models/verificationCode.model";
 import appAssert from "../utils/appAssert";
 import { oneYearFromNow } from "../utils/date";
 import jwt from "jsonwebtoken";
+import { refreshTokenSignOptions, signToken } from "../utils/jwt";
 
 export type CreateAccountParams = {
     email: string;
@@ -35,9 +36,11 @@ export const createAccount = async (data: CreateAccountParams) => {
         password: data.password,
     });
 
+    const userId = user._id;
+
     // create verification code
     const verificationCode = await VerificationCodeModel.create({
-        userId: user._id,
+        userId,
         type: VerificationCodeType.EmailVerification,
         expiresAt: oneYearFromNow(),
     });
@@ -46,24 +49,84 @@ export const createAccount = async (data: CreateAccountParams) => {
 
     // create session
     const session = await SessionModel.create({
-        userId: user._id,
+        userId,
         userAgent: data.userAgent
     });
 
-    // sign access token & refresh token
-    const refreshToken = jwt.sign(
-        {sessionId: session._id},
-        JWT_REFRESH_SECRET,
-        {
-            audience: ["user"],
-            expiresIn: "30d"
-        }
-    );
+    const sessionId = session._id;
 
-    const accessToken = jwt.sign(
+    // sign access token & refresh token
+    const refreshToken = signToken(
+        {sessionId},
+        refreshTokenSignOptions
+    )
+
+    const accessToken = signToken(
         {
-            userId: user._id,
-            sessionId: session._id
+            sessionId,
+            userId,
+        },
+    )
+
+    // return user & tokens
+    return {
+        user: user.omitPassword(),
+        accessToken,
+        refreshToken
+    }
+}
+
+export type LoginParams = {
+    email: string;
+    password: string;
+    userAgent?: string;
+}
+
+export const loginUser = async ({email,password,userAgent}: LoginParams) => {
+    // get the user by email
+    const user = await UserModel.findOne({email});
+    appAssert(
+        user,
+        UNAUTHORIZED,
+        "Invalid email or password"
+    )
+
+    // validate password from the request
+    const isValid = user.comparePassword(password);
+    appAssert(
+        isValid,
+        UNAUTHORIZED,
+        "Invalid email or password"
+    )
+    const userId = user._id;
+
+    // create session
+    const session = await SessionModel.create({
+        userId,
+        userAgent
+    });
+
+    const sessionInfo = {
+        sessionId: session._id
+    }
+
+    // sign access token & refresh token
+    const refreshToken = signToken(
+        sessionInfo,
+        refreshTokenSignOptions
+    )
+
+    const accessToken = signToken(
+        {
+            ...sessionInfo,
+            userId,
+        },
+    )
+    
+    jwt.sign(
+        {
+            ...sessionInfo,
+            userId,
         },
         JWT_SECRET,
         {
