b1c9a6001181b85cbf8558a13eaf2c0fc35f13d7
preethiviy <preethivirajyambem@gmail.com>
2024-09-11 13:28:32 +0530

    refresh token

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { loginHandler, logoutHandler, registerHandler } from "../controllers/auth.controller";
+import { loginHandler, logoutHandler, refreshHandler, registerHandler } from "../controllers/auth.controller";
 
 const authRoutes = Router();
 
 //prefix: /auth
 authRoutes.post("/register", registerHandler);
 authRoutes.post("/login", loginHandler);
+authRoutes.get("/refresh", refreshHandler);
 authRoutes.get("/logout", logoutHandler);
 
 export default authRoutes;
