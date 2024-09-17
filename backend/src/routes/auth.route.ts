14d7161163c4d551e15f2068469814a52c5c60cf
preethiviy <preethivirajyambem@gmail.com>
2024-09-17 22:56:11 +0530

    send password reset code

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginHandler, logoutHandler, refreshHandler, registerHandler, verifyEmailHandler } from "../controllers/auth.controller";
+import { loginHandler, logoutHandler, refreshHandler, registerHandler, sendPasswordResetHandler, verifyEmailHandler } from "../controllers/auth.controller";
 
 const authRoutes = Router();
 
@@ -9,5 +9,6 @@ authRoutes.post("/login", loginHandler);
 authRoutes.get("/refresh", refreshHandler);
 authRoutes.get("/logout", logoutHandler);
 authRoutes.get("/email/verify/:code", verifyEmailHandler);
+authRoutes.post("/password/forgot", sendPasswordResetHandler);
 
 export default authRoutes;
--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,15 +1,15 @@
 import { APP_ORIGIN, JWT_SECRET } from "../constants/env";
-import { CONFLICT, INTERNAL_SERVER_ERROR, NOT_FOUND, UNAUTHORIZED } from "../constants/http";
+import { CONFLICT, INTERNAL_SERVER_ERROR, NOT_FOUND, TOO_MANY_REQUESTS, UNAUTHORIZED } from "../constants/http";
 import VerificationCodeType from "../constants/verificationCodeTypes";
 import SessionModel from "../models/session.model";
 import UserModel from "../models/user.model";
 import VerificationCodeModel from "../models/verificationCode.model";
 import appAssert from "../utils/appAssert";
-import { ONE_DAY_MS, oneYearFromNow, thirtyDaysFromNow } from "../utils/date";
+import { fiveMinutesAgo, ONE_DAY_MS, oneHourFromNow, oneYearFromNow, thirtyDaysFromNow } from "../utils/date";
 import jwt from "jsonwebtoken";
 import { RefreshTokenPayload, refreshTokenSignOptions, signToken, verifyToken } from "../utils/jwt";
 import { sendMail } from "../utils/sendMail";
-import { getVerifyEmailTemplate } from "../utils/emailTemplates";
+import { getPasswordResetTemplate, getVerifyEmailTemplate } from "../utils/emailTemplates";
 
 export type CreateAccountParams = {
     email: string;
@@ -211,5 +211,42 @@ export const verifyEmail = async (code: string) => {
     // return user
     return {
         user: updatedUser.omitPassword()
+    }
+}
+
+export const sendPasswordResetEmail = async (email: string) => {
+    // get the user by id
+    const user = await UserModel.findOne({email});
+    appAssert(user, NOT_FOUND, "User not found");
+
+    // check email rate limit
+    const fiveMinAgo = fiveMinutesAgo();
+    const count = await VerificationCodeModel.countDocuments({
+        userId: user._id,
+        type: VerificationCodeType.PasswordReset,
+        createdAt: {$gt: fiveMinAgo},
+    })
+    appAssert(count <=1 , TOO_MANY_REQUESTS, "Too many requests, please try again later");
+
+    // create verification code
+    const expiresAt = oneHourFromNow();
+    const verificationCode = await VerificationCodeModel.create({
+        userId: user._id,
+        type: VerificationCodeType.PasswordReset,
+        expiresAt
+    })
+
+    // send verification email
+    const url = `${APP_ORIGIN}/password/reset?code=${verificationCode._id}&exp=${expiresAt.getTime()}`;
+    const {data, error} = await sendMail({
+        to: user.email,
+        ...getPasswordResetTemplate(url)
+    })
+    appAssert(data?.id, INTERNAL_SERVER_ERROR, `${error?.name} - ${error?.message}`)
+
+    // return success
+    return {
+        url,
+        emailId: data.id
     }
 }
